5806e104c863d6f8be20378e5babac3b3267b730
canelian <psy1884@gmail.com>
2022-08-31 10:37:36 +0900

    upup

--- bobaedream/js/main.js
+++
@@ -1,8 +0,0 @@
-// 헤더 메뉴
-$(function () {
-  $('.navigation #gnb > .gnb_ali').hover(function () {
-      $(this).find('.gnb_bul').stop().slideDown();
-  }, function () {
-      $(this).find('.gnb_bul').stop().slideUp();
-  });
-});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 let pageable = new Pageable("#container");
 
-=======
-let pageable = new Pageable("#container");
-
->>>>>>> 995f4fd31e55ea866095f5db39f530bbf98267d1
 console.log(pageable);
